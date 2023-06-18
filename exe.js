ee5a046e65a666071336f09089d5cc41b3f0ab91
Devina02 <115222060+Devina02@users.noreply.github.com>
2023-06-18 23:23:28 +0700

    exe2_20210140094

---
+++ exe.js
@@ -0,0 +1,36 @@
+function sendData(event) {
+    event.preventDefault();
+  
+    var name = document.getElementById("name").value;
+    var kartun = document.querySelector("input[name=kartun]:checked").value;
+    var pesan = document.getElementById("pesan").value;
+    var deskripsi = document.getElementById("deskripsi").value;
+    var birthdate = document.getElementById("birthdate").value;
+    var gender = document.getElementById("gender").value;
+  
+    var alertMessage =
+      "Nama: " + name +
+      "\nBirthdate: " + birthdate +
+      "\nJenis Kelamin: " + gender +
+      "\nKartun: " + kartun +
+      "\nDeskripsi: " + deskripsi +
+      "\nPesan: " + pesan;
+  
+    alert(alertMessage);
+  
+    var data = {
+      nama: name,
+      kartun: kartun,
+      pesan: pesan,
+      deskripsi: deskripsi,
+      birthdate: birthdate,
+      gender: gender
+    };
+  
+    localStorage.setItem("formData", JSON.stringify(data));
+  }
+  
+  function backpage() {
+    window.location.href = document.referrer;
+  }
+  
